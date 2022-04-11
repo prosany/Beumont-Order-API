dc4ea977625279247d636df19a13e7f7c1291e26
Mahabub Sunny <mahabubsany2@gmail.com>
2022-04-11 15:21:36 +0600

    Order Create Controller Added

--- Controllers/Order.Controller.js
+++ Controllers/Order.Controller.js
@@ -1,9 +1,11 @@
 const createError = require("http-errors");
+const Order = require("../Models/Order.Model");
 
 module.exports = {
   Orders: async (req, res, next) => {
     try {
-      res.send({ status: 1, orders: [] });
+      const orders = await Order.find({});
+      res.status(200).send({ status: 1, results: orders });
     } catch (error) {
       error.status = 404;
       next(error);
@@ -11,7 +13,10 @@ module.exports = {
   },
   CreateOrder: async (req, res, next) => {
     try {
-      res.send({ status: 1, orders: [] });
+      const order = new Order(req.body);
+      const savedOrder = await order.save();
+      if (!savedOrder) throw createError.BadRequest("Order Placed Failed");
+      res.send({ status: 1, message: "Order Placed Successful" });
     } catch (error) {
       next(error);
     }
